2985bf205fc88c69365dbf429197f3b663043cc3
Mike Beaumier <michael.beaumier@gmail.com>
2017-10-13 10:21:31 -0700

    slight refactor, now capturing more info

--- network_monitor.py
+++ network_monitor.py
@@ -5,15 +5,11 @@ import time
 import random
 import multiprocessing
 
-ping_list = [
-    'http://www.google.com',
-    'http://www.facebook.com',
-    'http://www.apple.com',
-    'http://www.microsoft.com'
-]
+with open("./ping_list.json", "r") as f:
+    _ping_list = json.load(f)
 
-if os.path.exists('network.log'):
-    with open('network.log','r') as f:
+if os.path.exists('network_log.json'):
+    with open('network_log.json','r') as f:
         try:
             network_log = json.load(f)
         except:
@@ -22,24 +18,36 @@ if os.path.exists('network.log'):
 else:
     network_log = []
 
-def check_response(url):
+def check_response(ping_entry):
+    url = ping_entry['url']
+    index = ping_entry['url_index']
     error = None
-    try:
-        resp_time = rq.get(url, timeout=5.0).elapsed.total_seconds()
-    except Exception as e:
-        resp_time = -1
-        error = type(e).__name__
+    error_message = None
+    resp_code = -1
+    resp_ok = False
+    resp_time = -1
 
-    return {'url':url, 'response_time':resp_time, 'unixtime':int(time.time()), 'error':error}
+    try:
+        res = rq.get(url, timeout=5.0)
+        resp_time = res.elapsed.total_seconds()
+        resp_code = res.status_code
+        resp_code = res.status_code
+        resp_ok = res.status_code == rq.codes.ok
+
+    except Exception as e:
+        error = type(e).__name__
+        error_message = repr(e)
+
+    return {'url':url, 'url_index':index, 'response_code':resp_code, 'response_ok':resp_ok, 'response_time':resp_time, 'unixtime':int(time.time()), 'error':error, 'error_message':error_message}
 
 while True:
     time.sleep(random.random()+1)
 
-    with multiprocessing.Pool(processes=len(ping_list)) as pool:
-        results = pool.map_async(check_response, ping_list)
+    with multiprocessing.Pool(processes=len(_ping_list)) as pool:
+        results = pool.map_async(check_response, _ping_list)
         results.wait()
         items = results.get()
         network_log += items
         print(json.dumps(items, indent=2))
-    with open('network.log', 'w') as outfile:
+    with open('network_log.json', 'w') as outfile:
         outfile.write(json.dumps(network_log, indent=2))
